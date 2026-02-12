9cfc37070abd23c981c2382892e43dd24dc1d313
jatin sharma <jatinsharma9871@gmail.com>
2026-02-12 15:27:30 +0530

    commit fine

--- api/filter.js
+++ api/filter.js
@@ -6,6 +6,13 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   const { minPrice, maxPrice, vendor, productType } = req.query;
 
