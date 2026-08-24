447701edae3fd4461e0697eaa4499124dfbb1785
jatin sharma <jatinsharma9871@gmail.com>
2026-08-24 12:27:48 +0530

    comit

--- api/filter.js
+++ api/filter.js
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE
 );
 
 export default async function handler(req, res) {
